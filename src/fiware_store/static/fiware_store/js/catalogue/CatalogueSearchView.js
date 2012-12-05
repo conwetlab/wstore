3a6400a484dda8743a45e61cab041b694fd04a6f
Francisco de la Vega <fdelavega@conwet.com>
2012-12-05 13:05:04 +0100

    Add user offerings search view and offering template

---
+++ src/fiware_store/static/fiware_store/js/catalogue/CatalogueSearchView.js
@@ -0,0 +1,45 @@
+
+(function () {
+
+    getUserOfferings = function getUserOfferings (target) {
+
+        var filter;
+
+        if (target.hash == '#provided-tab') {
+            filter = '?filter=provider&state=uploaded'; // TODO take into account published offerings
+        } else if (target.hash == '#purchased-tab'){
+            filter = '?filter=organization&state=purchased';
+        }
+        $.ajax({
+            type: "GET",
+            url: EndpointManager.getEndpoint('APPLICATION_COLLECTION') + filter,
+            dataType: 'json',
+            success: function(response) {
+                paintProvidedOfferings(target, response);
+            },
+            error: function(xhr) {
+                var msg = 'Error the server responds with code ' + xhr.status;
+                MessageManager.showMessage('Error', msg);
+            }
+
+        })
+    }
+
+    paintProvidedOfferings = function paintProvidedOfferings (target, data) {
+
+        $(target.hash).empty();
+        for (var i = 0; i < data.length; i++) {
+            var offering_elem = new OfferingElement(data[i]);
+
+            $.template('miniOfferingTemplate', $('#mini_offering_template'));
+            $.tmpl('miniOfferingTemplate', {
+                'name': offering_elem.getName(),
+                'organization': offering_elem.getOrganization(),
+                'logo': offering_elem.getLogo(),
+                'state': offering_elem.getState(),
+                'rating': offering_elem.getRating(),
+                'description': offering_elem.getShortDescription()
+            }).appendTo(target.hash);
+        };
+    }
+})();
---
+++ src/fiware_store/static/fiware_store/js/catalogue/OfferingElement.js
@@ -0,0 +1,87 @@
+
+(function () {
+
+	OfferingElement = function OfferingElement (offeringData) {
+	    this.name = offeringData.name;
+            this.organization = offeringData.owner_organization;
+            this.provider = offeringData.owner_admin_user;
+            this.version = offeringData.version;
+            this.logo = offeringData.image_url;
+            this.state = offeringData.state;
+            this.resources = offeringData.resources;
+            this.rating = offeringData.rating;
+            this.comments = offeringData.comments;
+            this.tags = offeringData.tags;
+            this.screenshots = offeringData.related_images;
+            this.shortDescription = offeringData.offering_description.shortDescription;
+            this.description = offeringData.offering_description.longDescription;
+            this.legal = offeringData.offering_description.legal;
+            this.sla = offeringData.offering_description.sla;
+            this.pricing = offeringData.offering_description.pricing;
+            // Add the usdl information
+        }
+
+        OfferingElement.prototype.getName = function getName () {
+            return this.name;
+        };
+
+        OfferingElement.prototype.getOrganization = function getOrganization () {
+            return this.organization;
+        };
+
+        OfferingElement.prototype.getProvider = function getProvider () {
+            return this.provider;
+        };
+
+        OfferingElement.prototype.getVersion = function getVersion () {
+            return this.version;
+        };
+
+        OfferingElement.prototype.getLogo = function getLogo () {
+            return this.logo;
+        };
+
+        OfferingElement.prototype.getState = function getState () {
+            return this.state;
+        };
+
+        OfferingElement.prototype.getResources = function getResources () {
+            return this.resources;
+        };
+
+        OfferingElement.prototype.getRating = function getRating () {
+            return this.rating;
+        };
+
+        OfferingElement.prototype.getComments = function getComments () {
+            return this.comments;
+        };
+
+        OfferingElement.prototype.getTags = function getTags () {
+            return this.tags;
+        };
+
+        OfferingElement.prototype.getScreenshots = function getScreenshots () {
+            return this.screenshots;
+        };
+
+        OfferingElement.prototype.getShortDescription = function getShortDescription () {
+            return this.shortDescription;
+        };
+
+        OfferingElement.prototype.getDescription = function getDescription () {
+            return this.description;
+        };
+
+        OfferingElement.prototype.getLegal = function getLegal () {
+            return this.legal;
+        };
+
+        OfferingElement.prototype.getSla = function getSla () {
+            return this.sla;
+        };
+
+        OfferingElement.prototype.getPricing = function getPricing () {
+            return this.pricing;
+        };
+})();
