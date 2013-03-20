65d3586daa2a012ece405330c02ebf77fbc1610e
Francisco de la Vega <fdelavega@conwet.com>
2013-03-20 17:51:41 +0100

    Add some tests to check searches

--- src/fiware_store/tests.py
+++ src/fiware_store/tests.py
@@ -3,3 +3,4 @@ from fiware_store.offerings.resource_tests import *
 from fiware_store.markets.tests import *
 from fiware_store.repositories.tests import *
 from fiware_store.contracting.tests import *
+from fiware_store.search.tests import *
