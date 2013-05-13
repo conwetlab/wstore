0518d739098fcc12d871837f44d6f9079c3587ef
Francisco de la Vega <fdelavega@conwet.com>
2013-05-13 13:59:40 +0200

    Add tests to check offering updates

--- src/wstore/offerings/offering_tests.py
+++ src/wstore/offerings/offering_tests.py
@@ -1,12 +1,14 @@
 import pymongo
 import os
 import base64
+import json
 
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.conf import settings
 
 from wstore.offerings import offerings_management
+from wstore.store_commons.utils.usdlParser import USDLParser
 from wstore.models import UserProfile
 from wstore.models import Offering
 from wstore.models import Marketplace
@@ -19,15 +21,27 @@ class FakeRepositoryAdaptor():
     _url = None
     _collection = None
 
-    def __init__(self, url, collection):
+    def __init__(self, url, collection=None):
         self._url = url
-        self._collection = collection
+        if collection != None:
+            self._collection = collection
 
-    def upload(self, name, cont, data):
-        return self._url + self._collection + '/' + name
+    def upload(self, cont, data, name=None):
+        if name != None:
+            url = self._url + self._collection + '/' + name
+        else:
+            url = self._url
+        return url
 
-    def delete(self, ser):
+    def delete(self, ser=None):
         pass
+
+    def download(self, name=None, content_type=None):
+        # Read the USDL from the file
+        path = os.path.join(settings.BASEDIR, 'wstore/test/')
+        f = open(os.path.join(path, 'test_usdl.rdf'), 'rb')
+        # Return the content
+        return f.read()
 
 
 class FakeMarketAdaptor():
@@ -66,6 +80,17 @@ class FakeSearchEngine():
         pass
 
 
+class FakeOfferingRollback():
+
+    _func = None
+
+    def __init__(self, func):
+        self._func = func
+
+    def __call__(self, provider, profile, json_data):
+        self._func(provider, profile, json_data)
+
+
 class OfferingCreationTestCase(TestCase):
 
     _db = None
@@ -82,6 +107,7 @@ class OfferingCreationTestCase(TestCase):
         # Capture repository calls
         offerings_management.RepositoryAdaptor = FakeRepositoryAdaptor
         offerings_management.SearchEngine = FakeSearchEngine
+        offerings_management.OfferingRollback = FakeOfferingRollback
         # loads test image
         path = os.path.join(settings.BASEDIR, 'wstore/test/')
         f = open(os.path.join(path, 'test_image.png'), 'rb')
@@ -214,6 +240,38 @@ class OfferingCreationTestCase(TestCase):
         contains = '/media/test_organization__test_offering__1.0/test_screen2.png' in content['related_images']
         self.assertTrue(contains)
 
+    def test_create_offering_usdl_url(self):
+
+        data = {
+            'name': 'test_offering',
+            'version': '1.0',
+            'repository': 'test_repository',
+            'image': {
+                'name': 'test_image.png',
+                'data': self._image,
+            },
+            'related_images': [],
+            'description_url': {
+                'content_type': 'application/rdf+xml',
+                'link': 'http://examplerep/v1/test_usdl'
+            }
+        }
+
+        user = User.objects.get(username='test_user')
+        profile = UserProfile.objects.get(user=user)
+        org = Organization.objects.get(name='test_organization')
+        profile.organization = org
+        profile.save()
+
+        offerings_management.create_offering(user, profile, data)
+        content = self._db.wstore_offering.find_one({"name": "test_offering"})
+
+        self.assertEqual(content['name'], 'test_offering')
+        self.assertEqual(content['version'], '1.0')
+        self.assertEqual(content['state'], 'uploaded')
+        self.assertEqual(content['image_url'], '/media/test_organization__test_offering__1.0/test_image.png')
+        self.assertEqual(content['description_url'], 'http://examplerep/v1/test_usdl')
+
     def test_create_offering_invalid_version(self):
         """
         Test the creating of an offering with invalid version format
@@ -344,6 +402,119 @@ class OfferingCreationTestCase(TestCase):
         self.assertEqual(msg, 'The offering already exists')
 
 
+class OfferingUpdateTestCase(TestCase):
+
+    fixtures = ['update.json']
+    _usdl = None
+
+    @classmethod
+    def setUpClass(cls):
+        offerings_management.RepositoryAdaptor = FakeRepositoryAdaptor
+
+        path = os.path.join(settings.BASEDIR, 'wstore/test/')
+        # loads test USDL
+        f = open(os.path.join(path, 'test_usdl.rdf'), 'rb')
+        cls._usdl = f.read()
+        super(OfferingUpdateTestCase, cls).setUpClass()
+
+    def test_basic_offering_update(self):
+        data = {
+            'offering_description': {
+                'content_type': 'application/rdf+xml',
+                'data': self._usdl
+            }
+        }
+        offering = Offering.objects.get(pk="61000aba8e15ac2115f022f9")
+
+        offerings_management.update_offering(offering, data)
+
+        offering = Offering.objects.get(pk="61000aba8e15ac2115f022f9")
+        usdl = offering.offering_description
+
+        parser = USDLParser(json.dumps(usdl), 'application/json')
+
+        usdl_content = parser.parse()
+
+        self.assertEqual(len(usdl_content['services_included']), 1)
+        service = usdl_content['services_included'][0]
+
+        self.assertEqual(service['name'], 'Map viewer')
+        self.assertEqual(service['vendor'], 'CoNWeT')
+
+        self.assertEqual(usdl_content['pricing']['title'], 'Map viewer free use')
+        self.assertEqual(len(usdl_content['pricing']['price_plans']), 1)
+
+        plan = usdl_content['pricing']['price_plans'][0]
+        self.assertEqual(plan['title'], 'Free use')
+
+    def test_offering_update_from_url(self):
+
+        data = {
+            'description_url': {
+                'content_type': 'application/rdf+xml',
+                'link': "http://examplerep/v1/test_usdl"
+            }
+        }
+        offering = Offering.objects.get(pk="61000aba8e15ac2115f022f9")
+
+        offerings_management.update_offering(offering, data)
+
+        offering = Offering.objects.get(pk="61000aba8e15ac2115f022f9")
+        usdl = offering.offering_description
+
+        parser = USDLParser(json.dumps(usdl), 'application/json')
+
+        usdl_content = parser.parse()
+
+        self.assertEqual(len(usdl_content['services_included']), 1)
+        service = usdl_content['services_included'][0]
+
+        self.assertEqual(service['name'], 'Map viewer')
+        self.assertEqual(service['vendor'], 'CoNWeT')
+
+        self.assertEqual(usdl_content['pricing']['title'], 'Map viewer free use')
+        self.assertEqual(len(usdl_content['pricing']['price_plans']), 1)
+
+        plan = usdl_content['pricing']['price_plans'][0]
+        self.assertEqual(plan['title'], 'Free use')
+
+    def test_offering_update_published(self):
+
+        offering = Offering.objects.get(pk="61000aba8e05ac2115f022f9")
+
+        error = False
+        msg = None
+        try:
+            offerings_management.update_offering(offering, {})
+        except Exception, e:
+            error = True
+            msg = e.message
+
+        self.assertTrue(error)
+        self.assertEquals(msg, 'The offering cannot be edited')
+
+    def test_offering_update_invalid_link(self):
+
+        data = {
+            'description_url': {
+                'content_type': 'application/rdf+xml',
+                'link': 'http://examplerep/v1/invalid'
+            }
+        }
+        offering = Offering.objects.get(pk="61000aba8e15ac2115f022f9")
+
+        error = False
+        msg = None
+        try:
+            offerings_management.update_offering(offering, data)
+        except Exception, e:
+            error = True
+            msg = e.message
+
+        self.assertTrue(error)
+        self.assertEquals(msg, 'The provided USDL URL is not valid')
+
+
 class OfferingRetrievingTestCase(TestCase):
 
     fixtures = ['get_prov.json']
@@ -425,9 +596,9 @@ class PurchasedOfferingRetrievingTestCase(TestCase):
         self.assertEqual(len(offerings[0]['bill']), 1)
         self.assertEqual(offerings[0]['bill'][0], '/media/bills/61005aba8e05ac2115f022f0.pdf')
         components = offerings[0]['offering_description']['pricing']['price_plans'][0]['price_components']
-        self.assertEqual(components[0]['title'],'price component 1')
+        self.assertEqual(components[0]['title'], 'price component 1')
         self.assertEqual(components[0]['renovation_date'], '1990-02-05 17:06:46')
-        self.assertEqual(components[1]['title'],'price component 2')
+        self.assertEqual(components[1]['title'], 'price component 2')
         self.assertEqual(components[1]['renovation_date'], '1990-02-05 17:06:46')
 
         self.assertEqual(offerings[1]['name'], 'test_offering2')
@@ -439,9 +610,9 @@ class PurchasedOfferingRetrievingTestCase(TestCase):
         self.assertEqual(len(offerings[1]['bill']), 1)
         self.assertEqual(offerings[1]['bill'][0], '/media/bills/61006aba8e05ac2115f022f0.pdf')
         components = offerings[1]['offering_description']['pricing']['price_plans'][0]['price_components']
-        self.assertEqual(components[0]['title'],'price component 1')
+        self.assertEqual(components[0]['title'], 'price component 1')
         self.assertEqual(components[0]['renovation_date'], '1990-02-05 17:06:46')
-        self.assertEqual(components[1]['title'],'price component 2')
+        self.assertEqual(components[1]['title'], 'price component 2')
         self.assertEqual(components[1]['renovation_date'], '1990-02-05 17:06:46')
 
     def test_get_published_offerings(self):
@@ -463,9 +634,9 @@ class PurchasedOfferingRetrievingTestCase(TestCase):
         self.assertEqual(offerings[0]['owner_admin_user_id'], 'test_user')
         self.assertEqual(offerings[0]['description_url'], 'http://testrepository/storeOfferingsCollection/test_organization__test_offering1__1.0')
         components = offerings[0]['offering_description']['pricing']['price_plans'][0]['price_components']
-        self.assertEqual(components[0]['title'],'price component 1')
+        self.assertEqual(components[0]['title'], 'price component 1')
         self.assertEqual(components[0]['renovation_date'], '1990-02-05 17:06:46')
-        self.assertEqual(components[1]['title'],'price component 2')
+        self.assertEqual(components[1]['title'], 'price component 2')
         self.assertEqual(components[1]['renovation_date'], '1990-02-05 17:06:46')
 
         self.assertEqual(offerings[1]['name'], 'test_offering2')
@@ -475,9 +646,9 @@ class PurchasedOfferingRetrievingTestCase(TestCase):
         self.assertEqual(offerings[1]['owner_admin_user_id'], 'test_user')
         self.assertEqual(offerings[1]['description_url'], 'http://testrepository/storeOfferingsCollection/test_organization__test_offering2__1.1')
         components = offerings[1]['offering_description']['pricing']['price_plans'][0]['price_components']
-        self.assertEqual(components[0]['title'],'price component 1')
+        self.assertEqual(components[0]['title'], 'price component 1')
         self.assertEqual(components[0]['renovation_date'], '1990-02-05 17:06:46')
-        self.assertEqual(components[1]['title'],'price component 2')
+        self.assertEqual(components[1]['title'], 'price component 2')
         self.assertEqual(components[1]['renovation_date'], '1990-02-05 17:06:46')
 
         self.assertEqual(offerings[2]['name'], 'test_offering3')
