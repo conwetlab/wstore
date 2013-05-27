df17bb4734bdd036f210033b023a743c1556e135
Francisco de la Vega <fdelavega@conwet.com>
2013-05-27 13:41:45 +0200

    Add tax address to organizations model

--- src/wstore/models.py
+++ src/wstore/models.py
@@ -24,6 +24,7 @@ class Organization(models.Model):
     name = models.CharField(max_length=50, unique=True)
     offerings_purchased = ListField()
     payment_info = DictField()
+    tax_address = DictField()
 
 
 class UserProfile(models.Model):
