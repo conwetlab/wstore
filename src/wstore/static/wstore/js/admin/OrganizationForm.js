788374327cc12560b18d4a80f16147f43cf11a66
fdelavega <fdelavega@conwet.com>
2014-03-07 11:23:05 +0100

    Include missing js header

--- src/wstore/static/wstore/js/admin/OrganizationForm.js
+++ src/wstore/static/wstore/js/admin/OrganizationForm.js
@@ -4,8 +4,8 @@
  * This file is part of WStore.
  *
  * WStore is free software: you can redistribute it and/or modify
- * it under the terms of the European Union Public Licence (EUPL) 
- * as published by the European Commission, either version 1.1 
+ * it under the terms of the European Union Public Licence (EUPL)
+ * as published by the European Commission, either version 1.1
  * of the License, or (at your option) any later version.
  *
  * WStore is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@
  * European Union Public Licence for more details.
  *
  * You should have received a copy of the European Union Public Licence
- * along with WStore.  
+ * along with WStore.
  * If not, see <https://joinup.ec.europa.eu/software/page/eupl/licence-eupl>.
  */
 
