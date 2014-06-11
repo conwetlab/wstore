078df3a2a2c2772d19f0724494f2d7b400e0e9af
fdelavega <fdelavega@conwet.com>
2014-06-11 18:08:22 +0200

    Improve searches delegating some features of pagination and sorting in Whoosh

--- src/wstore/search/search_engine.py
+++ src/wstore/search/search_engine.py
@@ -23,11 +23,12 @@ from __future__ import unicode_literals
 import os
 import json
 import rdflib
-from whoosh.fields import Schema, TEXT
+from whoosh.fields import Schema, TEXT, NUMERIC, DATETIME, KEYWORD
 from whoosh.index import create_in, open_dir
 from whoosh.qparser import QueryParser
+from whoosh import query
 
-from wstore.models import Offering
+from wstore.models import Offering, Purchase
 
 
 class SearchEngine():
@@ -38,6 +39,10 @@ class SearchEngine():
         self._index_path = index_path
 
     def _aggregate_text(self, offering):
+        """
+        Create a single string for creating the index by extracting text fields
+        from the USDL document of the offering
+        """
 
         usdl_document = offering.offering_description
         graph = rdflib.ConjunctiveGraph()
@@ -52,7 +57,20 @@ class SearchEngine():
 
         return text
 
+    def _aggregate_purchasers(self, offering):
+        purchases = Purchase.objects.filter(offering=offering)
+
+        result = ''
+        for p in purchases:
+            result += p.owner_organization.pk + ','
+
+        return result[:-1]
+
     def create_index(self, offering):
+        """
+        Create a document entry for the offering in the
+        search index
+        """
 
         # Check if the index already exists to avoid overwrite it
         if not os.path.exists(self._index_path) or os.listdir(self._index_path) == []:
@@ -61,7 +79,16 @@ class SearchEngine():
                 os.makedirs(self._index_path)
 
             # Create schema
-            schema = Schema(id=TEXT(stored=True), content=TEXT)
+            schema = Schema(
+                id=TEXT(stored=True),
+                owner=KEYWORD,
+                content=TEXT,
+                name=KEYWORD(sortable=True),
+                popularity=NUMERIC(int, decimal_places=2, sortable=True, signed=False),
+                date=DATETIME(sortable=True),
+                state=KEYWORD,
+                purchaser=KEYWORD
+            )
             # Create index
             index = create_in(self._index_path, schema)
         else:
@@ -74,11 +101,33 @@ class SearchEngine():
         # a single string in order to add a new document to the index
         text = self._aggregate_text(offering)
 
+        purchasers_text = self._aggregate_purchasers(offering)
+
+        from decimal import Decimal
         # Add the new document
-        index_writer.add_document(id=unicode(offering.pk), content=unicode(text))
+        index_writer.add_document(
+            id=unicode(offering.pk),
+            owner=unicode(offering.owner_organization.pk),
+            content=unicode(text),
+            name=unicode(offering.name),
+            popularity=Decimal(offering.rating),
+            date=offering.creation_date,
+            state=unicode(offering.state),
+            purchaser=purchasers_text
+        )
         index_writer.commit()
 
+    def update_index(self, offering):
+        """
+        Update the document of a concrete offering in the search index
+        """
+        pass
+
     def full_text_search(self, user, text, state=None, count=False, pagination=None, sort=None):
+        """
+        Performs a full text search over the search index allowing for counting, filtering
+        by state, paginating and sorting.
+        """
 
         if not os.path.exists(self._index_path) or os.listdir(self._index_path) == []:
             raise Exception('The index does not exist')
@@ -88,75 +137,74 @@ class SearchEngine():
 
         with index.searcher() as searcher:
 
-            # perform the query
-            query = QueryParser('content', index.schema).parse(unicode(text))
+            # Create the query
+            query_ = QueryParser('content', index.schema).parse(unicode(text))
 
-            search_result = searcher.search(query, limit=None)
+            # If an state has been defined filter the result
+            if state:
+                if state == 'all':  # All user owned offerings
+                    filter_ = query.Term('owner', unicode(user.userprofile.current_organization.pk))
+                elif state == 'purchased':  # Purchased offerings
+                    filter_ = query.Term('purchaser', user.userprofile.current_organization.pk)
+                elif state == 'uploaded' or state == 'deleted':  # Uploaded or deleted offerings owned by the user
+                    filter_ = query.Term('state', state) & query.Term('owner', unicode(user.userprofile.current_organization.pk))
+                else:
+                    raise ValueError('Invalid state')
+            else:
+                # If state is not included the default behaviour is returning
+                # published offerings
+                filter_ = query.Term('state', 'published')
+
+            # Create sorting params if needed
+            if sort:
+                if sort == 'popularity' or sort == 'date':
+                    reverse = True
+                elif sort == 'name':
+                    reverse = False
+                else:
+                    raise ValueError('Undefined sorting')
+
+            # If pagination has been defined, limit the results
+            if pagination:
+                # Validate pagination fields
+                if not isinstance(pagination, dict):
+                    raise TypeError('Invalid pagination type')
+
+                if not 'start' in pagination or not 'limit' in pagination:
+                    raise ValueError('Missing required field in pagination')
+
+                if not isinstance(pagination['start'], int) or not isinstance(pagination['limit'], int):
+                    raise TypeError('Invalid pagination params type')
+
+                if pagination['start'] < 1:
+                    raise ValueError('Start param must be higher than 0')
+
+                if pagination['limit'] < 0:
+                    raise ValueError('Limit param must be positive')
+
+                if sort:
+                    search_result = searcher.search_page(query_, pagination['start'], filter=filter_, pagelen=pagination['limit'], sortedby=sort, reverse=reverse)
+                else:
+                    search_result = searcher.search_page(query_, pagination['start'], filter=filter_, pagelen=pagination['limit'])
+            else:
+                if sort:
+                    search_result = searcher.search(query_, filter=filter_, limit=None, sortedby=sort, reverse=reverse)
+                else:
+                    search_result = searcher.search(query_, filter=filter_, limit=None)
+                
 
             result = []
             # The get_offering_info method is imported inside this method in order to avoid a cross-reference import error
             from wstore.offerings.offerings_management import get_offering_info
 
-            i = 0
-            for hit in search_result:
 
-                # Get the offerings
-                offering = Offering.objects.get(pk=hit['id'])
-                offering_info = get_offering_info(offering, user)
+            if not count:
+                for hit in search_result:
 
-                # filter the offerings
-                # if no state provided means that all published offerings are wanted
-                if state == None:
-                    if offering_info['state'] == 'published' or offering_info['state'] == 'purchased':
-                        if not count:
-                            result.append(offering_info)
-                        else:
-                            i += 1
-
-                elif state == 'purchased':
-                    if offering_info['state'] == 'purchased':
-                        if not count:
-                            result.append(offering_info)
-                        else:
-                            i += 1
-
-                elif state == 'all':
-                    if (offering.owner_admin_user == user) and \
-                    (offering.owner_organization == user.userprofile.current_organization):
-                        if not count:
-                            result.append(offering_info)
-                        else:
-                            i += 1
-                else:
-                    if (offering.owner_admin_user == user) and (offering_info['state'] == state) \
-                    and (offering.owner_organization == user.userprofile.current_organization):
-                        if not count:
-                            result.append(offering_info)
-                        else:
-                            i += 1
-
-            if count:
-                result = {'number': i}
-            elif pagination != None and len(result) > 0:
-                # Check if it is possible to retrieve the requested page
-                if pagination['start'] > len(result):
-                    raise Exception('Invalid page')
-
-                result = result[pagination['start'] - 1: (pagination['limit'] + (pagination['start'] - 1))]
-
-            # Check if is needed to sort the result
-            rev = True
-            if not count and sort:
-                if sort == 'name':
-                    rev = False
-
-                if sort == 'date':
-                    if state != 'all' or state != 'purchased':
-                        sort = 'creation_date'
-                    else:
-                        sort = 'publication_date'
-
-                # Sort the result
-                result = sorted(result, key=lambda off: off[sort], reverse=rev)
+                    # Get the offerings
+                    offering = Offering.objects.get(pk=hit['id'])
+                    result.append(get_offering_info(offering, user))
+            else:
+                result = {'count': len(search_result)}
 
         return result
