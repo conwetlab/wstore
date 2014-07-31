a8665127304b050fda8a4755760298477cdaefc7
fdelavega <fdelavega@conwet.com>
2014-07-31 15:10:17 +0200

    Add a class for managing revenue sharing models

--- src/wstore/rss_adaptor/tests.py
+++ src/wstore/rss_adaptor/tests.py
@@ -21,10 +21,12 @@
 import json
 from mock import MagicMock
 from urllib2 import HTTPError
+from nose_parameterized import parameterized
 
 from django.test import TestCase
+from django.conf import settings
 
-from wstore.rss_adaptor import rss_adaptor, expenditure_manager, rss_manager
+from wstore.rss_adaptor import rss_adaptor, expenditure_manager, rss_manager, model_manager
 from wstore.store_commons.utils.testing import mock_request
 
 
@@ -345,3 +347,91 @@ class ExpenditureManagerTestCase(TestCase):
         # Make the call
         self.manager.update_balance(charge, profile)
         self.manager._make_request.assert_called_with('PUT', 'http://testrss.com/expenditureLimit/balanceAccumulated/1', expected_data)
+
+
+class ModelManagerTestCase(TestCase):
+
+    tags = ('rs-models', )
+
+    @classmethod
+    def setUpClass(cls):
+        # Save used libraries
+        cls._old_RSS = rss_manager.RSS
+
+        # Create Mocks
+        cls.rss_mock = MagicMock()
+        cls.opener = MagicMock()
+        cls.mock_response = MagicMock()
+        cls.opener.open.return_value = cls.mock_response
+
+        rss_manager.RSS = MagicMock()
+        rss_manager.RSS.objects.get.return_value = cls.rss_mock
+        super(ModelManagerTestCase, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        rss_manager.RSS = cls._old_RSS
+        reload(rss_manager)
+        super(ModelManagerTestCase, cls).tearDownClass()
+
+    def setUp(self):
+        self.rss_mock.reset_mock()
+        self.rss_mock.host = 'http://testrss.com/'
+        self.manager = model_manager.ModelManager(self.rss_mock, 'accesstoken')
+        self.manager._make_request = MagicMock()
+        TestCase.setUp(self)
+
+    @parameterized.expand([
+        ('complete_model', {
+            'class': 'app',
+            'percentage': 20.0
+        }),
+        ('complete_provider',  {
+            'class': 'app',
+            'percentage': 20.0
+        }, 'test_user'),
+        ('missing_class', {
+            'percentage': 20.0
+        }, None, ValueError, 'Missing a required field in model info'),
+        ('missing_perc', {
+            'class': 'app'
+        }, None, ValueError, 'Missing a required field in model info'),
+        ('inv_data', ('app', 20.0), None, TypeError, 'Invalid type for model info'),
+        ('inv_class', {
+            'class': 7,
+            'percentage': 20.0
+        }, None, TypeError, 'Invalid type for class field'),
+        ('inv_percentage', {
+            'class': 'app',
+            'percentage': '20.0'
+        }, None, TypeError, 'Invalid type for percentage field'),
+        ('bigger_perc', {
+            'class': 'app',
+            'percentage': 102.0
+        }, None, ValueError, 'The percentage must be a number between 0 and 100')
+    ])
+    def test_create_model(self, name, data, provider=None, err_type=None, err_msg=None):
+
+        error = None
+        try:
+            self.manager.create_revenue_model(data, provider)
+        except Exception as e:
+            error = e
+
+        if not err_type:
+            self.assertEquals(error, None)
+            # Check calls
+            if provider:
+                exp_prov = provider
+            else:
+                exp_prov = settings.STORE_NAME.lower()
+
+            exp_data = {
+                'appProviderId': exp_prov,
+                'productClass': data['class'],
+                'percRevenueShare': data['percentage']
+            }
+            self.manager._make_request.assert_called_with('POST', 'http://testrss.com/fiware-rss/rss/rsModelsMgmt', exp_data)
+        else:
+            self.assertTrue(isinstance(error, err_type))
+            self.assertEquals(unicode(e), err_msg)
