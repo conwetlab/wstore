e09505d530b0798f1085a5b3b1f68749b94ccd89
fdelavega <fdelavega@conwet.com>
2014-09-23 16:11:53 +0200

    Add test to check createtags management comment

--- src/wstore/management/tests.py
+++ src/wstore/management/tests.py
@@ -30,7 +30,7 @@ from django.test import TestCase
 from django.conf import settings
 from django.core.management import call_command
 
-from wstore.management.commands import configureproject, createindexes
+from wstore.management.commands import configureproject, createindexes, createtags
 
 
 class ConfigureProjectTestCase(TestCase):
@@ -52,52 +52,31 @@ class ConfigureProjectTestCase(TestCase):
         pass
 
 
-class CreateIndexesTestCase(TestCase):
-
-    tags = ('management',)
+class IndexTestCase(TestCase):
 
     def setUp(self):
-        # Mock search engine
-        self.se_inst = MagicMock()
-        createindexes.SearchEngine = MagicMock()
-        createindexes.SearchEngine.return_value = self.se_inst
         # Mock the standard input
-        createindexes.stdin = MagicMock()
-        createindexes.stdin.readline.return_value = 'y '
+        self.tested_mod.stdin = MagicMock()
+        self.tested_mod.stdin.readline.return_value = 'y '
         # Mock rmtree
-        createindexes.rmtree = MagicMock()
-        # Mock offerings
-        createindexes.Offering = MagicMock()
-        self.offerings = [
-            'offering1',
-            'offering2',
-            'offering3'
-        ]
-        createindexes.Offering.objects.all.return_value = self.offerings
+        self.tested_mod.rmtree = MagicMock()
         TestCase.setUp(self)
-
-    def tearDown(self):
-        reload(createindexes)
-        TestCase.tearDown(self)
 
     def _invalid_option(self):
         def mock_rl():
-            createindexes.stdin.readline = MagicMock()
-            createindexes.stdin.readline.return_value = 'y '
+            self.tested_mod.stdin.readline = MagicMock()
+            self.tested_mod.stdin.readline.return_value = 'y '
             return 't '
 
-        createindexes.stdin.readline = mock_rl
+        self.tested_mod.stdin.readline = mock_rl
 
     def _canceled(self):
-        createindexes.stdin.readline.return_value = 'n '
+        self.tested_mod.stdin.readline.return_value = 'n '
 
-    @parameterized.expand([
-        ('no_input', False),
-        ('interactive',),
-        ('inter_inv', True, _invalid_option),
-        ('canceled', True, _canceled, False)
-    ])
-    def test_create_indexes(self, name, input_=True, side_effect=None, completed=True):
+    def manager_assertion(self):
+        pass
+
+    def _index_tst(self, info, input_=True, side_effect=None, completed=True):
 
         args = []
         opts = {}
@@ -107,22 +86,112 @@ class CreateIndexesTestCase(TestCase):
         if side_effect:
             side_effect(self)
 
-        call_command('createindexes', *args, **opts)
+        call_command(info['command'], *args, **opts)
 
         if completed:
             index_path = os.path.join(settings.BASEDIR, 'wstore')
-            index_path = os.path.join(index_path, 'search')
+            index_path = os.path.join(index_path, info['module'])
             index_path = os.path.join(index_path, 'indexes')
 
             # Check calls
-            createindexes.rmtree.assert_called_once_with(index_path, True)
+            self.tested_mod.rmtree.assert_called_once_with(index_path, True)
 
-            self.se_inst.create_index.assert_calls(self.offerings, True)
+            self.manager_assertion()
         else:
             called = True
             try:
-                createindexes.rmtree.assert_any_call()
+                self.tested_mod.rmtree.assert_any_call()
             except:
                 called = False
 
             self.assertFalse(called)
+
+
+class CreateIndexesTestCase(IndexTestCase):
+    tags = ('management',)
+
+    def __init__(self, methodName='runTest'):
+        self.tested_mod = createindexes
+        IndexTestCase.__init__(self, methodName=methodName)
+
+    def setUp(self):
+        # Mock search engine
+        self.se_inst = MagicMock()
+        createindexes.SearchEngine = MagicMock()
+        createindexes.SearchEngine.return_value = self.se_inst
+        # Mock offerings
+        self.tested_mod.Offering = MagicMock()
+        self.offerings = [
+            'offering1',
+            'offering2',
+            'offering3'
+        ]
+        self.tested_mod.Offering.objects.all.return_value = self.offerings
+        IndexTestCase.setUp(self)
+
+    def tearDown(self):
+        reload(createindexes)
+        IndexTestCase.tearDown(self)
+
+    def manager_assertion(self):
+        self.se_inst.create_index.assert_calls(self.offerings, True)
+        IndexTestCase.manager_assertion(self)
+
+    @parameterized.expand([
+        ('no_input', False),
+        ('interactive',),
+        ('inter_inv', True, IndexTestCase._invalid_option),
+        ('canceled', True, IndexTestCase._canceled, False)
+    ])
+    def test_create_indexes(self, name, input_=True, side_effect=None, completed=True):
+
+        info = {
+            'command': 'createindexes',
+            'module': 'search'
+        }
+        self._index_tst(info, input_=input_, side_effect=side_effect, completed=completed)
+
+
+class CreateTagIndexesTestCase(IndexTestCase):
+
+    tags = ('management',)
+
+    def __init__(self, methodName='runTest'):
+        self.tested_mod = createtags
+        IndexTestCase.__init__(self, methodName=methodName)
+
+    def setUp(self):
+        # Mock Tagging manager
+        self.tm_inst = MagicMock()
+        createtags.TagManager = MagicMock()
+        createtags.TagManager.return_value = self.tm_inst
+        # Mock offerings
+        self.offering = MagicMock()
+        self.offering.tags = ['tag1']
+        createtags.Offering = MagicMock()
+        createtags.Offering.objects.all.return_value = [
+            self.offering
+        ]
+        IndexTestCase.setUp(self)
+
+    def tearDown(self):
+        reload(createtags)
+        IndexTestCase.tearDown(self)
+
+    def manager_assertion(self):
+        self.tm_inst.update_tags.assert_called_once_with(self.offering, ['tag1'])
+        IndexTestCase.manager_assertion(self)
+
+    @parameterized.expand([
+        ('no_input',),
+        ('interactive',),
+        ('inter_inv', True, IndexTestCase._invalid_option),
+        ('canceled', True, IndexTestCase._canceled, False)
+    ])
+    def test_create_tag_indexes(self, name, input_=True, side_effect=None, completed=True):
+
+        info = {
+            'command': 'createtags',
+            'module': 'social'
+        }
+        self._index_tst(info, input_=input_, side_effect=side_effect, completed=completed)
