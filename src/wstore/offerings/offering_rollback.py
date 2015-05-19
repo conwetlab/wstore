bfbcafbd5298a6f5d4b03e194203abe7670ab963
fdelavega <fdelavega@conwet.com>
2015-05-19 19:01:40 +0200

    Update create offering rollback method to remove repository request

--- src/wstore/offerings/offering_rollback.py
+++ src/wstore/offerings/offering_rollback.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2013 CoNWeT Lab., Universidad Politécnica de Madrid
+# Copyright (c) 2013 - 2015 CoNWeT Lab., Universidad Politécnica de Madrid
 
 # This file is part of WStore.
 
@@ -20,20 +20,12 @@
 
 import os
 from urllib2 import HTTPError
-from urlparse import urljoin
 
 from django.conf import settings
-from wstore.repository_adaptor.repositoryAdaptor import RepositoryAdaptor
-from wstore.models import Offering, Repository, Resource
+from wstore.models import Offering, Resource
 
 
 def rollback(provider, profile, json_data, msg):
-
-    # Check the created exceptions in order to determine if is
-    # necessary to remove something
-    if msg == 'Missing required fields' or msg == 'Invalid version format' \
-    or msg == 'The offering already exists':
-        return
 
     # Check files
     dir_name = profile.current_organization.name + '__' + json_data['name'] + '__' + json_data['version']
@@ -50,15 +42,8 @@ def rollback(provider, profile, json_data, msg):
     # Check if the offering has been created
     offering = Offering.objects.filter(owner_organization=profile.current_organization, name=json_data['name'], version=json_data['version'])
 
-    remove = False
     if len(offering) > 0:
         offering = offering[0]
-
-        # If the offerings has been created means that the USDL is
-        # uploaded in the repository
-        if 'offering_description' in json_data:
-            remove = True
-            url = offering.description_url
 
         # Check if the offering has been bound
         if len(offering.resources):
@@ -68,27 +53,6 @@ def rollback(provider, profile, json_data, msg):
                 re.save()
 
         offering.delete()
-    else:
-        # Check if the usdl was uploaded before the exception
-        if 'offerings_description' in json_data:
-            repository = Repository.objects.get(name=json_data['repository'])
-            repository_adaptor = RepositoryAdaptor(repository.host, 'storeOfferingCollection')
-            offering_id = profile.current_organization.name + '__' + json_data['name'] + '__' + json_data['version']
-
-            uploaded = True
-            try:
-                repository_adaptor.download(name=offering_id, content_type=json_data['offering_description']['content_type'])
-            except HTTPError:
-                uploaded = False
-
-            if uploaded:
-                remove = True
-                url = urljoin(repository.host, 'storeOfferingCollection')
-                url = urljoin(url, offering_id)
-
-    if remove:
-        repository_adaptor = RepositoryAdaptor(url)
-        repository_adaptor.delete()
 
 
 class OfferingRollback():
