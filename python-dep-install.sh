b218d2a04e1f28aec4bde216c8d08a6bce2aacf1
fdelavega <fdelavega@conwet.com>
2015-06-02 16:01:06 +0200

    Set versions of python dependencies in installation scripts

--- python-dep-install.sh
+++ python-dep-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # Download and install Python dependencies
-pip install lxml "rdflib>=3.2.0" "pymongo==2.8"
+pip install "lxml==3.4.4" "rdflib==4.2.0" "pymongo==2.8"
 pip install https://github.com/django-nonrel/django/archive/nonrel-1.4.zip
 pip install https://github.com/django-nonrel/djangotoolbox/archive/toolbox-1.4.zip
 pip install https://github.com/django-nonrel/mongodb-engine/archive/mongodb-engine-1.4-beta.zip
 pip install https://github.com/RDFLib/rdflib-jsonld/archive/master.zip
 
 pip install https://github.com/conwetlab/paypalpy/archive/master.zip
-pip install nose django-nose
+pip install "nose==1.3.6" "django-nose==1.4"
 
-pip install django-social-auth
-pip install django-crontab
-pip install Whoosh
-pip install Stemming
+pip install "django-social-auth==0.7.28"
+pip install "django-crontab==0.6.0"
+pip install "Whoosh==2.7.0"
+pip install "Stemming==1.0.1"
 pip install requests
 pip install regex
